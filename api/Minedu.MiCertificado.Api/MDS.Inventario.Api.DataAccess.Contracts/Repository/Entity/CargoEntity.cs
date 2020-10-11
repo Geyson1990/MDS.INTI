855cf36a7b6c8594c2347445b1ea4e00c3102353
Geyson <xpgeyson@gmail.com>
2020-10-11 18:35:52 -0500

    GRVB: Método Cargo

---
+++ api/Minedu.MiCertificado.Api/MDS.Inventario.Api.DataAccess.Contracts/Repository/Entity/CargoEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDS.Inventario.Api.DataAccess.Contracts.Entities.Certificado
+{
+    public class CargoEntity : cargo
+    {
+
+    }
+}
---
+++ api/Minedu.MiCertificado.Api/MDS.Inventario.Api.DataAccess.Contracts/UnitOfWork/IUnitOfWorkCargo.cs
@@ -0,0 +1,12 @@
+﻿using MDS.Inventario.Api.DataAccess.Contracts.Entities.Certificado;
+using Minedu.Comun.IData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDS.Inventario.Api.DataAccess.Contracts.UnitOfWork
+{
+    public partial interface IUnitOfWork : IBaseUnitOfWork
+    {
+        Task<int> Insertar(CargoEntity request);
+    }
+}
